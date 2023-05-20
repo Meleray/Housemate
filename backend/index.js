2bc218e041fdb318c38ebe04758c390535324ae6
Meleray <konovma@gmail.com>
2023-05-20 15:18:18 +0200

    Add simple server code

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const express = require('express');
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended: True}))
+
+app.listen(process.env.PORT || 80, () => {
+  console.log('Express server is up and running on http://localhost:80/');
+});
