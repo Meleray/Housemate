ae9cf2f991ea45a64fee0c2ea59cd2cf37c5cf99
Meleray <konovma@gmail.com>
2023-05-20 15:55:45 +0200

    Add simple routing

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 require("dotenv").config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const routing = require("./routes/Routing");
 
 const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+app.use("/api", routing)
 
 app.listen(process.env.PORT || 80, () => {
   console.log('Express server is up and running on http://localhost:80/');
---
+++ backend/routes/Routing.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.get("/test", (req, res) => {
+    // Write the database access here
+})
+
+module.exports = router
