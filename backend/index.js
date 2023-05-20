89bc796be3d5f792439fcad58aedd71fa38eec5e
Meleray <konovma@gmail.com>
2023-05-20 15:33:13 +0200

    Fix error in index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.json())
-app.use(express.urlencoded({extended: True}))
+app.use(express.urlencoded({extended: true}))
 
 app.listen(process.env.PORT || 80, () => {
   console.log('Express server is up and running on http://localhost:80/');
