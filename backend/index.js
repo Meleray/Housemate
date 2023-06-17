532b988627fffdefc5a0cbf0f529165e640900a2
Meleray <konovma@gmail.com>
2023-06-17 14:23:40 +0200

    Allow CORS requests in backend

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const cors = require("cors");
 const express = require('express');
 const bodyParser = require('body-parser');
 const routing = require("./routes/routing");
@@ -6,7 +7,7 @@ const houseDb = require("./database/database");
 const cookieParser = require("cookie-parser");
 
 const app = express();
-
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cookieParser())
