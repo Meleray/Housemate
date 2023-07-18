303f0007a496ef7adc4d8c07001104ea218ade35
Andrian Raja Naibaho <rajanaibaho@Andrians-MBP.fritz.box>
2023-07-18 15:53:38 +0200

    fix select user error

--- frontend/src/components/TaskSystem/TaskSelectUser.jsx
+++ frontend/src/components/TaskSystem/TaskSelectUser.jsx
@@ -5,7 +5,8 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import { useState, useEffect } from 'react';
-import { ApiFindSpace, ApiFindUserById, router_auth } from "../../constants";
+import { ApiFindSpaceMembers, ApiFindUserById, router_auth } from "../../constants";
+import {getSafe} from "../../utils";
 
 export default function TaskSelectUser({ setAssignedUser }) {
   const [responsibleUser, setResponsibleUser] = React.useState('');
@@ -18,38 +19,17 @@ export default function TaskSelectUser({ setAssignedUser }) {
   };
 
   useEffect(() => {
-    async function fetchData() {
+    async function fetchUserData() {
       const result = await router_auth.request({
         method: 'POST',
-        url: ApiFindSpace,
-        headers: { 'content-type': 'application/json' },
-        data: { spaceId: localStorage.getItem('spaceId') },
+        url: ApiFindSpaceMembers,
+        headers: {'content-type': 'application/json',},
+        data: {spaceId: getSafe(localStorage, "spaceId")},
       });
-      const members = result.data.spaceMembers;
-      console.log("inviteMemberList:", members);
-
-      // Fetch user information for each member
-      const memberPromises = members.map(async (member) => {
-        const userResult = await router_auth.request({
-          method: 'POST',
-          url: ApiFindUserById,
-          headers: { 'content-type': 'application/json' },
-          data: { userId: member.memberId },
-        });
-        const user = userResult.data;
-        return {
-          memberId: member.memberId,
-          userName: user.userName,
-        };
-      });
-
-      // Wait for all member promises to resolve
-      const memberData = await Promise.all(memberPromises);
-      setMemberList(memberData);
-      console.log("Member data:", memberData);
-    }
-
-    fetchData();
+      setMemberList(result.data)
+      console.log("members", result.data)
+      }
+      void fetchUserData();
   }, []);
 
   return (
